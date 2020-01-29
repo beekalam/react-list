5ec6308779cad4037e3bb1325a4a6235ed0e9bdb
beekalam <beekalam@gmail.com>
2020-01-29 20:37:45 +0330

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+class Status extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            likes: 0
+        }
+    }
+
+    like(){
+        this.states.likes++;
+    }
+
+    render() {
+        return (
+            <div className="status">
+                <p>{this.props.mytext}</p>
+                <p>
+                    <button onClick={this.like()}>{this.state.likes} Like</button>
+                </p>
+            </div>
+        );
+    }
+
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <Status mytext="Wow, I had the most delicious luch today."/>
+
+                <Status mytext="There was not traffic today."/>
+
+                <Status mytext="finally finished  that big project."/>
+
+                <Status mytext="happy learning reactjs."/>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
